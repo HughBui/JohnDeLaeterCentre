1db949d777c191b47ef78a7191d357691a51082e
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-12 17:58:28 +1000

    edit home

--- wp-content/themes/twentytwelve/home.php
+++ wp-content/themes/twentytwelve/home.php
@@ -30,9 +30,9 @@
 			</div>
 		</header><!--  end header section  -->
 
-			<section class="caption" style="margin-top:20px;">
+			<section class="caption" style="margin-top:10px;">
 				<h2 class="caption">OUR FACILITIES</h2>
-				<span style="max-width:800px;"><h3 class="properties">John de Laeter Centre provides unique quantitative data on the way the Earth works, elucidating the changing environment we live in and the nature and origin of resources we depend on.</h3></span>
+				<h3 class="caption">John de Laeter Centre provides unique quantitative data on the way the Earth works, elucidating the changing environment we live in and the nature and origin of resources we depend on.</h3>
 			</section>
 	</section><!--  end hero section  --> 
 
