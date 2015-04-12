b4e169287bc3437da882fc54e9e80ab7f7f7c729
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-12 15:42:34 +1000

    edit home

--- wp-content/themes/twentytwelve/home.php
+++ wp-content/themes/twentytwelve/home.php
@@ -1,1 +1,15 @@
-<h1>hello</h1>
+<html>
+<head>
+	<style>
+		body {
+			background-image: url("img/FacilitiesHomePageTemplate.jpg");
+			background-repeat: no-repeat;
+			background-position: center top;
+			background-size: cover;
+			height: 1700px;
+		}
+	</style>
+
+</head>
+<body></body>
+</html>
