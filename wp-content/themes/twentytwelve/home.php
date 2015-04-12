1d4b381c56a4dfdd987e5ebb9d6e08b9dd788eaf
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-13 01:05:47 +1000

    edit home

--- wp-content/themes/twentytwelve/home.php
+++ wp-content/themes/twentytwelve/home.php
@@ -226,7 +226,7 @@
 	</section>	<!--  end listing section  -->
 
 	<footer>
-		<table style="width:100%;">
+		<table style="width:100%; border:1px">
 			<tr>
 				<td><img src="<?php bloginfo(stylesheet_directory); ?>/img/address.jpg"/></td>
 				<td>
