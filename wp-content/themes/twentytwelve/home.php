5cd22baeac8b158ad5ed4ea415995733c41b41b7
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-12 23:14:56 +1000

    add home

--- wp-content/themes/twentytwelve/home.php
+++ wp-content/themes/twentytwelve/home.php
@@ -119,7 +119,7 @@
 								Dr Allen Kennedy
 							</p>
 							<p style="color:white; display:none;">
-								<br/><br/><br/><br/>
+								<br/><br/><br/>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ante quam, sit amet faucibus metus aliquet sit amet. Pellentesque mollis suscipit arcu eget lobortis.
 							</p>
 						</div>
@@ -135,7 +135,7 @@
 								Dr Allen Kennedy
 							</p>
 							<p style="color:white; display:none;">
-								<br/><br/><br/><br/>
+								<br/><br/><br/>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ante quam, sit amet faucibus metus aliquet sit amet. Pellentesque mollis suscipit arcu eget lobortis.
 							</p>
 						</div>
@@ -183,7 +183,7 @@
 								Dr Allen Kennedy
 							</p>
 							<p style="color:white; display:none;">
-								<br/><br/><br/><br/>
+								<br/><br/><br/>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ante quam, sit amet faucibus metus aliquet sit amet. Pellentesque mollis suscipit arcu eget lobortis.
 							</p>
 						</div>
@@ -199,7 +199,7 @@
 								Dr Allen Kennedy
 							</p>
 							<p style="color:white; display:none;">
-								<br/><br/><br/><br/>
+								<br/><br/><br/>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ante quam, sit amet faucibus metus aliquet sit amet. Pellentesque mollis suscipit arcu eget lobortis.
 							</p>
 						</div>
@@ -215,7 +215,7 @@
 								Dr Allen Kennedy
 							</p>
 							<p style="color:white; display:none;">
-								<br/><br/><br/><br/>
+								<br/><br/><br/>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ante quam, sit amet faucibus metus aliquet sit amet. Pellentesque mollis suscipit arcu eget lobortis.
 							</p>
 						</div>
@@ -230,36 +230,14 @@
 			<ul>
 				<li class="links">
 					<ul>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Support</a></li>
-						<li><a href="#">Terms</a></li>
-						<li><a href="#">Policy</a></li>
-						<li><a href="#">Contact</a></li>
+						<li><a href="#">STAFF PORTAL</a></li>
+						<li><a href="#">LINKS</a></li>
+						<li><a href="#">SCIENCE LINKS</a></li>
+						<li><a href="#">BOARD OF DIRECTORS</a></li>
 					</ul>
 				</li>
-
-				<li class="links">
-					<ul>
-						<li><a href="#">Appartements</a></li>
-						<li><a href="#">Houses</a></li>
-						<li><a href="#">Villas</a></li>
-						<li><a href="#">Mansions</a></li>
-						<li><a href="#">...</a></li>
-					</ul>
-				</li>
-
-				<li class="links">
-					<ul>
-						<li><a href="#">New York</a></li>
-						<li><a href="#">Los Anglos</a></li>
-						<li><a href="#">Miami</a></li>
-						<li><a href="#">Washington</a></li>
-						<li><a href="#">...</a></li>
-					</ul>
-				</li>
-
 				<li class="about">
-					<p>La Casa is real estate minimal html5 website template, designed and coded by pixelhint, tellus varius, dictum erat vel, maximus tellus. Sed vitae auctor ipsum</p>
+					<p>Contact to us Via social:</p>
 					<ul>
 						<li><a href="http://facebook.com/pixelhint" class="facebook" target="_blank"></a></li>
 						<li><a href="http://twitter.com/pixelhint" class="twitter" target="_blank"></a></li>
@@ -271,7 +249,7 @@
 		</div>
 
 		<div class="copyrights wrapper">
-			Copyright © 2015 <a href="http://pixelhint.com" target="_blank" class="ph_link" title="Download more free Templates">Pixelhint.com</a>. All Rights Reserved.
+
 		</div>
 	</footer><!--  end footer  -->
 	
