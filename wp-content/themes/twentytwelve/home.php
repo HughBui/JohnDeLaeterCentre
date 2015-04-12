a2a9e965a2ea06d90b885966f3cddd4258aabc0e
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-13 02:31:17 +1000

    add home

--- wp-content/themes/twentytwelve/home.php
+++ wp-content/themes/twentytwelve/home.php
@@ -227,13 +227,13 @@
 
 	<footer>
 	
-		<div style="float:left;">
+		<div style="float:left; background: #303030;">
 					<a href="#">STAFF PORTAL</a><br/>
 					<a href="#">LINKS</a><br/>
 					<a href="#">SCIENCE LINKS</a><br/>
 					<a href="#">BOARD OF DIRECTORS</a><br/>
 		</div>
-		<div style="float:left;">
+		<div style="float:left; background: #303030;">
 					<p>Contact to us Via social:</p>
 					<a href="" target="_blank"><img src="<?php bloginfo(stylesheet_directory); ?>/img/in.png"/></a>
 					<a href="" target="_blank"><img src="<?php bloginfo(stylesheet_directory); ?>/img/twitter.png"/></a>
