eb8df24c1e2484fc6569b5e8546f95d76d95bc50
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-29 00:01:33 +1000

    facilities

--- wp-content/themes/twentytwelve/facilities.php
+++ wp-content/themes/twentytwelve/facilities.php
@@ -143,7 +143,7 @@
 				<script>
 					$(function() {
 						if($(window).width() <= 800){
-							$(".property_details img").css("position", "initial");
+							$(".property_details > img").css("position", "initial");
 						}
 					});
 				</script>
