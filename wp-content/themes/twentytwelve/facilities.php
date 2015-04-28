ecc889ee37722999da8841cc5412c4d01d51fa92
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-28 17:45:23 +1000

    facilities

--- wp-content/themes/twentytwelve/facilities.php
+++ wp-content/themes/twentytwelve/facilities.php
@@ -56,15 +56,15 @@
 	</section>
 	<!--  end header section  -->
 	
-	<div class="wrapper">
-	<section class="caption">
-		<h2 class="caption">APPLICATIONS</h2><br/>
-		<p class="caption">
-			The TIMS instrument, in combination with isotope dilution methodology, is<br/>
-			widely used in chemical metrology for the calibration of isotopic standards, and<br/>
-			the calculation of isotopic abundances and atomic weights. 
-		</p>
-	</section>
+	<div class="wrapper" style="position:relative;">
+		<section class="caption">
+			<h2 class="caption">APPLICATIONS</h2><br/>
+			<p class="caption">
+				The TIMS instrument, in combination with isotope dilution methodology, is<br/>
+				widely used in chemical metrology for the calibration of isotopic standards, and<br/>
+				the calculation of isotopic abundances and atomic weights. 
+			</p>
+		</section>
 	</div>
 	
 	<!--  grid section  -->
