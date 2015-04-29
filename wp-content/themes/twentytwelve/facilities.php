5dd56e46b98f29d55d8f12e0a0443e17bd8ba529
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-29 18:49:06 +1000

    facilities

--- wp-content/themes/twentytwelve/facilities.php
+++ wp-content/themes/twentytwelve/facilities.php
@@ -173,7 +173,7 @@
 				</li>
 				<li>
 					<div class="property_details right_bottom_borders" style="height:400px;">
-						<div style="background: url(<?php bloginfo(stylesheet_directory); ?>/img/5.jpg) no-repeat; background-size: 100% auto;">
+						<div style="background: url(<?php bloginfo(stylesheet_directory); ?>/img/5.jpg) no-repeat; background-size: 100%;">
 							<br/><br/><br/><br/>
 							<h1>
 								DETECTION OF LONG RANGE POLLUTION TRANSPORT ON AEROSOLS.
