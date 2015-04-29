24cf48bf20462778f07d57f207804109e94bfe76
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-29 19:20:11 +1000

    facilities

--- wp-content/themes/twentytwelve/facilities.php
+++ wp-content/themes/twentytwelve/facilities.php
@@ -200,7 +200,7 @@
 				</li>
 			</ul>
 		</div>
-		<div style="width: 200px; height:100px; text-align:center; border: 1px solid #dadada;">SEE MORE</div>
+		<div class="hero blank" style="width: 150px; height:50px; text-align:center; border: 1px solid #dadada; margin: 0 auto;">SEE MORE</div>
 	</section>
 	<!--  end grid section  -->
 
