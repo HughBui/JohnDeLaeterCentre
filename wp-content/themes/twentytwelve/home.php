6d736315bd97e6d75ac47f65fef0d98d126ca18f
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-30 19:14:11 +1000

    home menu

--- wp-content/themes/twentytwelve/home.php
+++ wp-content/themes/twentytwelve/home.php
@@ -24,10 +24,19 @@
 				<a href="#" class="hamburger"></a>
 				<nav>
 					<ul>
-						<li style="border-bottom: 2px solid #ffffff;"><a href="#">Facilities</a></li>
+						<li id="selected"><a href="#">Facilities</a></li>
 						<li><a href="#">Instruments</a></li>
 						<li><a href="#">Research</a></li>
 					</ul>
+					<script>
+						$("header nav ul li:not(#selected)").hover(
+							function() {
+								$(this).css('border-bottom','1px solid #ffffff'); 
+							}, function() {
+								$(this).css('border-bottom',''); 
+							}
+						);
+					</script>
 					<a href="#" class="login_btn">Contact</a>
 				</nav>
 			</div>
