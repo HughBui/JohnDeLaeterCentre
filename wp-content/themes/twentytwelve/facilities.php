a991ccf4eacca9b4397351bac269eabc4f7cc546
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-30 14:24:17 +1000

    facilities

--- wp-content/themes/twentytwelve/facilities.php
+++ wp-content/themes/twentytwelve/facilities.php
@@ -326,25 +326,25 @@
 		<span class="fading_line"></span>
 	</section>
 	
-	<section class="listings" style="height 200px;">
+	<section class="listings" style="height 100px;">
 		<div class="wrapper">
 			<ul class="properties_list">
 				<li>
-					<div class="property_details" style="height:300px; text-align:center; border:none;">
+					<div class="property_details" style="height:100px; text-align:center; border:none;">
 						<div class="hero blank btn" style="width: 150px; height:50px; line-height:50px; text-align:center; border: 1px solid #dadada; margin: 0 auto;">
 							<span style="color: #666464; font-family: 'lato-bold', Helvetica, Arial, sans-serif; font-size: 16px; font-weight: bold;">SEE MORE</span>
 						</div>
 					</div>
 				</li>
 				<li>
-					<div class="property_details" style="height:300px; text-align:center;border:none;">
+					<div class="property_details" style="height:100px; text-align:center;border:none;">
 						<div class="hero blank btn" style="width: 150px; height:50px; line-height:50px; text-align:center; border: 1px solid #dadada; margin: 0 auto;">
 							<span style="color: #666464; font-family: 'lato-bold', Helvetica, Arial, sans-serif; font-size: 16px; font-weight: bold;">SEE MORE</span>
 						</div>
 					</div>
 				</li>
 				<li>
-					<div class="property_details" style="height:300px; text-align:center;border:none;">
+					<div class="property_details" style="height:100px; text-align:center;border:none;">
 						<div class="hero blank btn" style="width: 150px; height:50px; line-height:50px; text-align:center; border: 1px solid #dadada; margin: 0 auto;">
 							<span style="color: #666464; font-family: 'lato-bold', Helvetica, Arial, sans-serif; font-size: 16px; font-weight: bold;">SEE MORE</span>
 						</div>
