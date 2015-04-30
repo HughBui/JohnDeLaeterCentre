7c93d403709ae81ac35a40df49ac37520cf5d5e6
hieutrongbui12 <hieutrongbui12@gmail.com>
2015-04-30 12:48:19 +1000

    facilities

--- wp-content/themes/twentytwelve/facilities.php
+++ wp-content/themes/twentytwelve/facilities.php
@@ -297,7 +297,7 @@
 				</li>
 				<li>
 					<div class="property_details" style="height:400px;">
-						<div style="margin: 0 auto; border: 1px solid #dadada;"><img class="img-round" src="<?php bloginfo(stylesheet_directory); ?>/img/6.JPG"/></div>
+						<img class="img-round" src="<?php bloginfo(stylesheet_directory); ?>/img/6.JPG"/>
 						<script>
 						$(document).ready(function() {
 							$('.img-round').roundImage();
